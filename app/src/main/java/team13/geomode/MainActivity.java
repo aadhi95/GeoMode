76e6177cc2dcf9ff8a249eecf4d9b9fe65f49b9d
aadhithya <aadhi95balaji@gmail.com>
2017-01-04 23:56:30 +0530

    hey

--- app/src/main/java/team13/geomode/MainActivity.java
+++ app/src/main/java/team13/geomode/MainActivity.java
@@ -1,13 +1,21 @@
 package team13.geomode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void usecurloc(View v){
+        Intent i=new Intent(this,MapsActivity.class);
+        startActivity(i);
+        //hello
     }
 }
