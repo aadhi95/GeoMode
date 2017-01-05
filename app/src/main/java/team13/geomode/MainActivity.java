fdebbc3143c82fb1567043b776b2edda76d1bf9f
aadhithya <aadhi95balaji@gmail.com>
2017-01-05 11:00:41 +0530

    changed the google maps activity to adjust to current location

--- app/src/main/java/team13/geomode/MainActivity.java
+++ app/src/main/java/team13/geomode/MainActivity.java
@@ -1,102 +1,23 @@
 package team13.geomode;
 
-import android.*;
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
-    TextView ln, lo;
-    LocationManager lm;
-    String provider;
-    Location l;
-    protected LocationManager locationManager;
-    protected LocationListener locationListener;
-    protected Context context;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ln = (TextView) findViewById(R.id.t1);
-        lo = (TextView) findViewById(R.id.t2);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},11);
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        Criteria c = new Criteria();
-        provider = lm.getBestProvider(c, true);
-        Toast.makeText(this,provider, Toast.LENGTH_SHORT).show();
-        l = lm.getLastKnownLocation(provider);
-        if(l!=null)
-        {
-            //get latitude and longitude of the location
-            //     ln.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-            double lng=l.getLongitude();
-            double lat=l.getLatitude();
-            //display on text view
-            ln.setText(""+lng);
-            lo.setText(""+lat);
-        }
-        else
-        {
-            ln.setText("No Provider");
-            lo.setText("No Provider");
-        }
+
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 11: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
     public void usecurloc(View v){
         Intent i=new Intent(this,MapsActivity.class);
-        i.putExtra("lat",l.getLatitude());
-        i.putExtra("lon",l.getLongitude());
         startActivity(i);
-        //hello
-        //world
-        //
     }
     public void usegmap(View v)
     {
@@ -104,26 +25,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        double lng=l.getLongitude();
-        double lat=l.getLatitude();
-        ln.setText(""+lng);
-        lo.setText(""+lat);
-    }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
 }
--- app/src/main/java/team13/geomode/MapsActivity.java
+++ app/src/main/java/team13/geomode/MapsActivity.java
@@ -1,8 +1,17 @@
 package team13.geomode;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,10 +21,17 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements
+        LocationListener,
         GoogleMap.OnMyLocationButtonClickListener,
         OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback {
-
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    TextView ln, lo;
+    LocationManager lm;
+    String provider;
+    Location l;
     private GoogleMap mMap;
 
     @Override
@@ -26,6 +42,23 @@ public class MapsActivity extends FragmentActivity implements
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, 11);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        Criteria c = new Criteria();
+        provider = lm.getBestProvider(c, true);
+        Toast.makeText(this, provider, Toast.LENGTH_SHORT).show();
+        l = lm.getLastKnownLocation(provider);
     }
 
 
@@ -41,17 +74,74 @@ public class MapsActivity extends FragmentActivity implements
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (l != null) {
+            LatLng ll = new LatLng(l.getLatitude(), l.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(ll).title("Marker in Current Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
+        }
+    }
 
-        // Add a marker in Sydney and move the camera
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 11: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-        LatLng sydney = new LatLng(getIntent().getDoubleExtra("lat",0), getIntent().getDoubleExtra("lon",0));
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
 
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
     public boolean onMyLocationButtonClick() {
         return false;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(ll).title("Marker in Current Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        switch (status) {
+            case LocationProvider.AVAILABLE:
+                Toast.makeText(this, provider + " available", Toast.LENGTH_SHORT).show();
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                Toast.makeText(this, provider + " out of service", Toast.LENGTH_SHORT).show();
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                Toast.makeText(this, provider + " unavailable", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
